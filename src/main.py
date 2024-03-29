b1f4d9cffc89512b093c2390ecbcb75882099bf8
amgs <amgs>
2024-03-29 01:57:23 +0000

    Format code.

--- src/main.py
+++ src/main.py
@@ -1,20 +1,21 @@
 import datetime
 import io
+import os
 from typing import Annotated
+
+import pandas as pd
+from dotenv import load_dotenv
 from fastapi import FastAPI, Header, Request
 from fastapi.responses import RedirectResponse, StreamingResponse
-from starlette import status
 from pymongo import MongoClient
-import pandas as pd
-import os
-from dotenv import load_dotenv
+from starlette import status
 
 load_dotenv()
 
-MONGO_USR=os.getenv("MONGO_USR")
-MONGO_PWD=os.getenv("MONGO_PWD")
-MONGO_HOST=os.getenv("MONGO_HOST")
-MONGO_APP_NAME=os.getenv("MONGO_APP_NAME")
+MONGO_USR = os.getenv("MONGO_USR")
+MONGO_PWD = os.getenv("MONGO_PWD")
+MONGO_HOST = os.getenv("MONGO_HOST")
+MONGO_APP_NAME = os.getenv("MONGO_APP_NAME")
 
 MONGO_URI = f"mongodb+srv://{MONGO_USR}:{MONGO_PWD}@{MONGO_HOST}/?retryWrites=true&w=majority&appName={MONGO_APP_NAME}"
 
@@ -26,11 +27,14 @@ def get_collection(database: str, collection: str):
     db = client[database]
     return db[collection]
 
-def get_data(project: str=""):
+
+def get_data(project: str = ""):
     records = get_collection("redirect", "records")
     data = []
     if project != "":
-        cursor = records.find({"project": project}, {"_id": 0, "timestamp": 1, "ip_address": 1, "url": 1})
+        cursor = records.find(
+            {"project": project}, {"_id": 0, "timestamp": 1, "ip_address": 1, "url": 1}
+        )
         data = [
             {
                 "timestamp": record["timestamp"],
@@ -43,12 +47,12 @@ def get_data(project: str=""):
 
 
 @app.get("/json")
-async def json(project: str=""):
+async def json(project: str = ""):
     return get_data(project)
 
 
 @app.get("/csv", response_class=StreamingResponse)
-async def csv(project: str=""):
+async def csv(project: str = ""):
     df = pd.DataFrame(get_data(project))
     stream = io.StringIO()
     df.to_csv(stream, index=False)
@@ -56,12 +60,14 @@ async def csv(project: str=""):
     response.headers["Content-Disposition"] = "attachment; filename=data.csv"
     return response
 
+
 @app.get("/ping")
 def ping():
     return {"ping": "pong"}
 
+
 @app.get("/")
-async def index(request: Request, project:str="", url: str = ""):
+async def index(request: Request, project: str = "", url: str = ""):
     current_time = datetime.datetime.now()
     ip_address = request.client.host
     user_agent = request.headers.get("user-agent", "unknown")
